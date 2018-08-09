e4b4628064ac8e9c478cbce0fd121bf7d0813c8f
Pieter Bogaerts <pieterbog@gmail.com>
2018-08-09 22:24:08 +0200

    Bootstrapped the project for a headstart tomorrow.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # telfort-alexa
 Amazon Alexa bot to retrieve data from Mijn Telfort.
 
-This is a demo project created for demo purposes at Telfort.
+## Instant developer experience
+1. Install [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/installing.html)
+2. [Configure](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html) the CLI
+3. Make sure you have [Docker](https://www.docker.com/) installed on your machine
+4. Run `npm install`
+
+**This is a demo project created for demo purposes.**
---
+++ handler.js
@@ -0,0 +1,51 @@
+const Alexa = require('ask-sdk-core');
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = login();
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+    
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+    }
+};
+
+module.exports.hello = skillBuilder
+	.addRequestHandlers(
+		LaunchRequestHandler,
+		SessionEndedRequestHandler
+	)
+	.addErrorHandlers(ErrorHandler)
+	.lambda();
